2022d0cf389f2ef07274984b31fbc55f68ffd8c8
小猪 <xiaomo_0258@icloud.com>
2020-11-22 19:17:40 +0800

    [更新] 更新镜像仓库

--- deploy.sh
+++ deploy.sh
@@ -1,56 +1,74 @@
 #!/bin/bash
 
 # 项目名称
-export COMPOSE_PROJECT_NAME="ek_teacher_web"
+export COMPOSE_PROJECT_NAME="ek_web"
+export COMPOSE_FILE="./docker/docker-compose.yml"
 
-# 获取短版本号
-GITCOMMITHASH=`git rev-parse --short HEAD`
-export GITHASH=${GITCOMMITHASH}
+# 部署版本号
+if [ x"$2" = x ]; then 
+  export VERSIONTAG="latest"
+else
+  export VERSIONTAG=$2
+fi
 
 # 容器服务
-services=("teacher_web")
+services=("ek_teacher_web")
 
 version(){
-  echo "=======项目名称:${COMPOSE_PROJECT_NAME}  版本号：${GITHASH}========"
+  tips "项目名称:${COMPOSE_PROJECT_NAME}  版本号：${VERSIONTAG}"
 }
 
 start(){
-  version
-  echo "======== 正在构建docer-compose ========"
-  docker-compose up --build -d
-  # 垃圾回收
-  docker image prune -f
-  echo "==== 完成 docker-compose 构建与运行 ===="
+  tips "正在运行服务"
+  docker-compose up -d --force-recreate
+  tips "完成运行服务"
+}
+
+down(){
+  tips "正在移除服务"
+  for i in ${services[@]}  
+  do 
+  docker ps -a | grep "${i}" | awk '{print $1}' | xargs docker rm -f 
+  docker images | grep "${i}" | awk '{print $1":"$2}' | xargs docker rmi -f
+  done
+  tips "完成移除服务"
 }
 
 stop(){
-  version
-  echo "======== 正在停止docer-compose ========"
-  for i in ${services[@]}  
-  do  
-  docker ps | grep "${i}" | awk '{print $1}' | xargs docker stop && docker ps -a | grep "${i}" | awk '{print $1}' | xargs docker rm && docker images | grep "${i}" | awk '{print $1":"$2}' | xargs docker rmi -f
-  done
-  echo "======== 完成docer-compose停止 ========"
+  tips "正在停止服务"
+  docker-compose stop
+  tips "完成停止服务"
 }
 
 restart(){
-  stop
-  start
+  tips "正在重启服务"
+  docker-compose restart
+  tips "完成重启服务"
 }
 
 usage(){
-    echo "Usage: deploy.sh [start|stop|restart|version]"
+    echo "Usage: deploy.sh [start|stop|down|restart|version] [version]"
     exit 1
 }
 
+tips(){
+  echo "======== ${1} ========"
+}
 case "$1" in
     "start")
+      version
       start
       ;;
     "stop")
+      version
       stop
       ;;
+    "down")
+      version
+      down
+      ;;
     "restart")
+      version
       restart
       ;;
     "version")
